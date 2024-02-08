bacce78f5bcf34db2f634415f0b5471d93bc376b
samuelmahecha <112221181+samuelmahecha@users.noreply.github.com>
2024-02-08 18:04:17 -0500

    Punto1

--- wait-notify-excercise/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ wait-notify-excercise/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -3,45 +3,67 @@ package edu.eci.arsw.primefinder;
 import java.util.LinkedList;
 import java.util.List;
 
-public class PrimeFinderThread extends Thread{
+public class PrimeFinderThread extends Thread {
 
-	
-	int a,b;
-	
-	private List<Integer> primes;
-	
-	public PrimeFinderThread(int a, int b) {
-		super();
-                this.primes = new LinkedList<>();
-		this.a = a;
-		this.b = b;
-	}
+    int a, b;
 
-        @Override
-	public void run(){
-            for (int i= a;i < b;i++){						
-                if (isPrime(i)){
-                    primes.add(i);
-                    System.out.println(i);
+    private List<Integer> primes;
+    private Object lock;
+    private boolean paused;
+
+    public PrimeFinderThread(int a, int b, Object lock) {
+        super();
+        this.primes = new LinkedList<>();
+        this.a = a;
+        this.b = b;
+        this.lock = lock;
+        this.paused = false;
+    }
+
+    @Override
+    public void run() {
+        for (int i = a; i < b; i++) {
+            synchronized (lock) {
+                if (paused) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
-	}
-	
-	boolean isPrime(int n) {
-	    boolean ans;
-            if (n > 2) { 
-                ans = n%2 != 0;
-                for(int i = 3;ans && i*i <= n; i+=2 ) {
-                    ans = n % i != 0;
-                }
-            } else {
-                ans = n == 2;
+            if (isPrime(i)) {
+                primes.add(i);
+                System.out.println(i);
             }
-	    return ans;
-	}
+        }
+    }
 
-	public List<Integer> getPrimes() {
-		return primes;
-	}
-	
+    boolean isPrime(int n) {
+        boolean ans;
+        if (n > 2) {
+            ans = n % 2 != 0;
+            for (int i = 3; ans && i * i <= n; i += 2) {
+                ans = n % i != 0;
+            }
+        } else {
+            ans = n == 2;
+        }
+        return ans;
+    }
+
+    public List<Integer> getPrimes() {
+        return primes;
+    }
+
+    public void pauseThread() {
+        paused = true;
+    }
+
+    public void resumeThread() {
+        synchronized (lock) {
+            paused = false;
+            lock.notify();
+        }
+    }
 }
